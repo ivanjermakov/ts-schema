782be48c707d02dcdd47f448298b3d6f97096bf2
ivanjermakov <ivanjermakov1@gmail.com>
2025-04-28 01:35:01 +0200

    Generate schema

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 extern crate swc_ecma_parser;
 use std::{
-    collections::HashMap,
+    collections::{BTreeMap, BTreeSet, HashMap},
     env::args,
     io::{Read, stdin},
 };
 
 use anyhow::{Context, Result, anyhow};
+use schemars::schema::{
+    InstanceType, ObjectValidation, RootSchema, Schema, SchemaObject, SingleOrVec, StringValidation,
+};
 use swc_common::sync::Lrc;
 use swc_common::{
     FileName, SourceMap,
     errors::{ColorConfig, Handler},
 };
-use swc_ecma_ast::{Ident, ImportDecl, TsTypeAliasDecl};
+use swc_ecma_ast::{ImportDecl, TsTypeAliasDecl};
 use swc_ecma_parser::{Parser, StringInput, Syntax, TsSyntax, lexer::Lexer};
 use swc_ecma_visit::{Visit, VisitWith};
 
@@ -67,13 +70,15 @@ fn main() -> Result<()> {
         Err(e) => return Err(anyhow!("Syntax error: {:?}", e.into_kind())),
     };
 
-    // eprintln!("{:#?}", module);
-
     let mut type_visitor = TypeVisitor {
         type_defs: HashMap::new(),
     };
     module.visit_with(&mut type_visitor);
     eprintln!("{:?}", type_visitor.type_defs.keys().collect::<Vec<_>>());
+
+    let schema = make_schema(&type_name, &type_visitor.type_defs)?;
+    let output = serde_json::to_string(&schema)?;
+    println!("{}", output);
 
     Ok(())
 }
@@ -82,4 +87,56 @@ fn read_stdin() -> Result<String> {
     let mut input = String::new();
     stdin().read_to_string(&mut input)?;
     Ok(input)
+}
+
+fn make_schema(
+    _type_name: &str,
+    _type_defs: &HashMap<String, TsTypeAliasDecl>,
+) -> Result<RootSchema> {
+    let schema = RootSchema {
+        meta_schema: Some("http://json-schema.org/draft-07/schema".into()),
+        schema: SchemaObject {
+            metadata: None,
+            instance_type: None,
+            format: None,
+            enum_values: None,
+            const_value: None,
+            subschemas: None,
+            number: None,
+            string: None,
+            array: None,
+            object: Some(Box::new(ObjectValidation {
+                max_properties: None,
+                min_properties: None,
+                required: BTreeSet::new(),
+                properties: vec![(
+                    "x".into(),
+                    Schema::Object(SchemaObject {
+                        metadata: None,
+                        instance_type: Some(SingleOrVec::Single(Box::new(InstanceType::String))),
+                        format: None,
+                        enum_values: None,
+                        const_value: None,
+                        subschemas: None,
+                        number: None,
+                        string: Some(Box::new(StringValidation::default())),
+                        array: None,
+                        object: None,
+                        reference: None,
+                        extensions: BTreeMap::new(),
+                    }),
+                )]
+                .into_iter()
+                .collect::<BTreeMap<_, _>>(),
+                pattern_properties: BTreeMap::new(),
+                additional_properties: None,
+                property_names: None,
+            })),
+            reference: None,
+            extensions: BTreeMap::new(),
+        },
+        definitions: BTreeMap::new(),
+    };
+
+    Ok(schema)
 }
