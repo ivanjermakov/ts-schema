cf4cdeffcda1994535dfc71097057a1d0ad700d1
ivanjermakov <ivanjermakov1@gmail.com>
2025-04-28 18:43:35 +0200

    Generate schema based on input file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate swc_ecma_parser;
 use std::{
+    any::type_name,
     collections::{BTreeMap, HashMap},
     env::args,
     io::{Read, stdin},
@@ -9,12 +10,16 @@ use anyhow::{Context, Result, anyhow};
 use schemars::schema::{
     InstanceType, ObjectValidation, RootSchema, Schema, SchemaObject, SingleOrVec,
 };
-use swc_common::sync::Lrc;
+use swc_common::{AstNode, sync::Lrc};
 use swc_common::{
     FileName, SourceMap,
     errors::{ColorConfig, Handler},
 };
-use swc_ecma_ast::{ImportDecl, TsTypeAliasDecl};
+use swc_ecma_ast::{
+    ImportDecl, TsKeywordType, TsKeywordTypeKind, TsPropertySignature,
+    TsType::{self, TsTypeLit},
+    TsTypeAliasDecl, TsTypeAnn, TsTypeElement, TsTypeRef,
+};
 use swc_ecma_parser::{Parser, StringInput, Syntax, TsSyntax, lexer::Lexer};
 use swc_ecma_visit::{Visit, VisitWith};
 
@@ -32,7 +37,6 @@ impl Visit for TypeVisitor {
         // for specifier in &decl.specifiers {
         //     if let swc_ecma_ast::ImportSpecifier::Named(named) = specifier {
         //         let Ident { sym, .. } = &named.local;
-        //         eprintln!("{}: {:#?}", sym, decl);
         //     }
         // }
         // decl.visit_children_with(self);
@@ -74,9 +78,8 @@ fn main() -> Result<()> {
         type_defs: HashMap::new(),
     };
     module.visit_with(&mut type_visitor);
-    eprintln!("{:?}", type_visitor.type_defs.keys().collect::<Vec<_>>());
 
-    let schema = make_schema(&type_name, &type_visitor.type_defs)?;
+    let schema = make_root_schema(&type_name, &type_visitor.type_defs)?;
     let output = serde_json::to_string(&schema)?;
     println!("{}", output);
 
@@ -89,34 +92,106 @@ fn read_stdin() -> Result<String> {
     Ok(input)
 }
 
-fn make_schema(
-    _type_name: &str,
-    _type_defs: &HashMap<String, TsTypeAliasDecl>,
+type Depenedencies = Vec<String>;
+
+fn make_root_schema(
+    type_name: &str,
+    type_defs: &HashMap<String, TsTypeAliasDecl>,
 ) -> Result<RootSchema> {
-    let schema = RootSchema {
+    let mut definitions = BTreeMap::new();
+    let mut dependencies: Depenedencies = vec![type_name.to_string()];
+    while let Some(type_name) = dependencies.pop() {
+        let (schema, deps) = make_schema(&type_name, type_defs)?;
+        definitions.insert(type_name, schema);
+        for dep in deps {
+            if !dependencies.contains(&dep) {
+                dependencies.push(dep)
+            }
+        }
+    }
+    Ok(RootSchema {
         meta_schema: Some("http://json-schema.org/draft-07/schema".into()),
         schema: SchemaObject {
-            instance_type: Some(SingleOrVec::Single(Box::new(InstanceType::Object))),
-            object: Some(Box::new(ObjectValidation {
-                properties: {
-                    vec![(
-                        "x".into(),
-                        Schema::Object(SchemaObject {
-                            instance_type: Some(SingleOrVec::Single(Box::new(
-                                InstanceType::String,
-                            ))),
-                            ..Default::default()
-                        }),
-                    )]
-                    .into_iter()
-                    .collect::<BTreeMap<_, _>>()
-                },
-                ..Default::default()
-            })),
+            reference: Some(format!("#/definitions/{type_name}")),
             ..Default::default()
         },
-        ..Default::default()
-    };
+        definitions,
+    })
+}
 
-    Ok(schema)
+fn make_schema(
+    type_name: &str,
+    type_defs: &HashMap<String, TsTypeAliasDecl>,
+) -> Result<(Schema, Depenedencies)> {
+    {
+        let instance_type = match type_name {
+            "string" => Some(InstanceType::String),
+            "boolean" => Some(InstanceType::Boolean),
+            "number" => Some(InstanceType::Number),
+            _ => None,
+        };
+        if instance_type.is_some() {
+            return Ok((
+                Schema::Object(SchemaObject {
+                    instance_type: instance_type.map(|i| SingleOrVec::Single(Box::new(i))),
+                    ..Default::default()
+                }),
+                vec![],
+            ));
+        }
+    }
+    let mut deps = vec![];
+    let type_def = type_defs
+        .get(type_name)
+        .context(format!("no type def with name `{type_name}`"))?;
+    let object = match type_def.type_ann.as_ref() {
+        TsTypeLit(ts_type_lit) => {
+            let mut object_val = ObjectValidation {
+                ..Default::default()
+            };
+            for member in &ts_type_lit.members {
+                match member {
+                    TsTypeElement::TsPropertySignature(TsPropertySignature {
+                        key,
+                        type_ann,
+                        ..
+                    }) => {
+                        let member_name = key.clone().ident().context("not ident")?.sym.to_string();
+                        let ts_type_ann: &TsTypeAnn = type_ann.as_ref().unwrap();
+                        let member_type = match *ts_type_ann.type_ann {
+                            TsType::TsTypeRef(TsTypeRef { ref type_name, .. }) => type_name
+                                .clone()
+                                .ident()
+                                .context("not ident")?
+                                .sym
+                                .to_string(),
+                            TsType::TsKeywordType(TsKeywordType { kind, .. }) => match kind {
+                                TsKeywordTypeKind::TsNumberKeyword => "number".into(),
+                                TsKeywordTypeKind::TsBooleanKeyword => "boolean".into(),
+                                TsKeywordTypeKind::TsStringKeyword => "string".into(),
+                                _ => todo!("{kind:?}"),
+                            },
+                            ref t => todo!("{t:?}"),
+                        };
+                        object_val.properties.insert(
+                            member_name.clone(),
+                            Schema::Object(SchemaObject {
+                                reference: Some(format!("#/definitions/{member_type}")),
+                                ..Default::default()
+                            }),
+                        );
+                        deps.push(member_type)
+                    }
+                    _ => todo!("{member:?}"),
+                };
+            }
+            Schema::Object(SchemaObject {
+                instance_type: Some(SingleOrVec::Single(Box::new(InstanceType::Object))),
+                object: Some(Box::new(object_val)),
+                ..Default::default()
+            })
+        }
+        t => todo!("{t:?}"),
+    };
+    Ok((object, deps))
 }
